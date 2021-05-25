2fdbc6cc220d39bc183ba1a781fc88d9a7dc2219
AzaharaGV <azahara1359@gmail.com>
2021-05-25 10:47:14 +0200

    Favorites

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const search = document.querySelector(".js-userInput");
 const buttonSearch = document.querySelector(".js-button");
 const resultContainer = document.querySelector(".js-placeResult");
+const favoriteContainer = document.querySelector(".js-favorites");
 let searchResult = [];
 let favoritesShow = [];
 let defaultImage = "//via.placeholder.com/210x295/ffffff/666666/?text=TV";
@@ -14,8 +15,11 @@ function handlerButtonSearch(event) {
     .then((data) => {
       searchResult = data;
       introElementsByDom();
+      renderFav();
     });
 }
+console.log(searchResult);
+getFromLocalStorage();
 
 // INTRODUZCO EN HTM LOS ELEMENTOS CON APPENCHILD Y SE HACEN VISIBLES
 function introElementsByDom() {
@@ -23,8 +27,8 @@ function introElementsByDom() {
   for (const itemSerie of searchResult) {
     const cards = document.createElement("ul");
     resultContainer.appendChild(cards);
-    cards.classList = "allSeries";
     const littleCards = document.createElement("li");
+    littleCards.setAttribute("id", itemSerie.show.id);
     cards.appendChild(littleCards);
     littleCards.setAttribute("class", "js-favoriteSelect");
     const allInfoCards = document.createTextNode(itemSerie.show.name);
@@ -52,5 +56,51 @@ buttonSearch.addEventListener("click", handlerButtonSearch);
 
 function favoriteSelect(event) {
   const favoriteSerieSelect = event.currentTarget;
-  favoriteSerieSelect.classList.toggle("js-favoriteSelect");
+  favoriteSerieSelect.classList.toggle("SuperFavoriteSelect");
+  const serieId = parseInt(event.currentTarget.id);
+  const seriFavId = searchResult.find(
+    (nuevaFavoritos) => nuevaFavoritos.show.id === serieId
+  );
+  favoritesShow.push(seriFavId);
+  localStorageFavorite();
+
+  renderFav();
+}
+
+//Recoger los favoritos
+//Pintar los favoritos
+function renderFav() {
+  favoriteContainer.innerHTML = "";
+  const favCards = document.createElement("ul");
+  favoriteContainer.appendChild(favCards);
+  for (const itemSerie of favoritesShow) {
+    const favLittleCards = document.createElement("li");
+    favCards.appendChild(favLittleCards);
+    favLittleCards.setAttribute("class", "favoriteSelectRender");
+    favLittleCards.setAttribute("id", itemSerie.show.id);
+    const tittleFavSerie = document.createElement("h3");
+    favLittleCards.appendChild(tittleFavSerie);
+    const allInfoFavCards = document.createTextNode(itemSerie.show.name);
+    tittleFavSerie.appendChild(allInfoFavCards);
+    const allInfoFavCardsImage = document.createElement("img");
+    favLittleCards.appendChild(allInfoFavCardsImage);
+    if (itemSerie.show.image === null) {
+      allInfoFavCardsImage.src = defaultImage;
+    } else {
+      allInfoFavCardsImage.src = itemSerie.show.image.medium;
+    }
+    //SuperFavoriteSelect();
+  }
+}
+console.log(renderFav);
+
+//Almaceno datos en el localStorage
+
+function localStorageFavorite() {
+  localStorage.setItem("", "");
+}
+
+function getFromLocalStorage() {
+  // Hay algo en el localStrorage?
+  // Si -> Sacar localStorage, parse, asignarselo a favoritesShow y llmar a renderFav()
 }
